3666f89257527e6bc84c27057144d39f28425585
cloudtypeguide <108320412+cloudtypeguide@users.noreply.github.com>
2025-12-30 14:31:37 +0900

    Update GuestRepository.java

--- src/main/java/io/cloudtype/waitlist/repository/GuestRepository.java
+++ src/main/java/io/cloudtype/waitlist/repository/GuestRepository.java
@@ -2,7 +2,11 @@ package io.cloudtype.waitlist.repository;
 
 import io.cloudtype.waitlist.model.Guest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
 public interface GuestRepository extends JpaRepository<Guest, Long> {
-
+    // 🔴 [추가] "A룸"의 "오늘 날짜" 예약만 몽땅 가져오는 명령어
+    List<Guest> findByRoomNameAndDate(String roomName, String date);
 }
